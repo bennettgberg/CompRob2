fb4eaf658949158c8ec3e58b3627c0b74f9b9421
chen19960615 <chen19960615@gmail.com>
2018-11-13 03:04:59 -0500

    testing publisher for controls

--- comprobfall2018-hw2/new_files/ackermann_publisher.py
+++ comprobfall2018-hw2/new_files/ackermann_publisher.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
+import sys
 from std_msgs.msg import String
-from ackermann_msgs.msg import AckermannDrive
 from std_msgs.msg import Float64
-from gazebo_msgs.srv import SetModelState, GetModelState, ModelState
+from ackermann_msgs.msg import AckermannDrive
+import ackermann_controller
+# from gazebo_msgs.srv import SetModelState, GetModelState, ModelState
 
 def ackermann_publisher(speed, steering_angle):
-	pub = rospy.Publisher("ackermann_cmd", AckermannDrive, self.ackermann_cmd_cb, queue_size=1)
+	pub = rospy.Publisher("ackermann_cmd", AckermannDrive, queue_size=1)
 	rospy.init_node('ackermann_controller', anonymous=True)
 	rate = rospy.Rate(10) # 10hz
 	while not rospy.is_shutdown():
@@ -15,31 +17,39 @@ def ackermann_publisher(speed, steering_angle):
 		state = AckermannDrive()
 		state.speed = speed
 		state.steering_angle = steering_angle
-		rospy.loginfo(state)
+		# rospy.loginfo(state)
 		pub.publish(state)
 		rate.sleep();
 
-def model_state_publisher(pose, twist):
-	pub = rospy.Publisher("/gazebo/set_model_state,", ModelState, queue_size=1)
-	rospy.init_node('model_state_controller', anonymous=True)
-	rate = rospy.Rate(10) # 10hz
-	while not rospy.is_shutdown():
+# def model_state_publisher(pose, twist):
+# 	pub = rospy.Publisher("/gazebo/set_model_state,", ModelState, queue_size=1)
+# 	rospy.init_node('model_state_controller', anonymous=True)
+# 	rate = rospy.Rate(10) # 10hz
+# 	while not rospy.is_shutdown():
 		
-		model_state = ModelState()
-		model_state.model_name = "ackermann_vehicle"
-		model_state.pose = pose
-		model_state.twist = twist
-		model_state.reference_frame = "world"
-		# model_state.pose.orientation.x = tf.transformations.quaternion_from_euler(0,0,steering_angle)[0]
-		# model_state.pose.orientation.y = tf.transformations.quaternion_from_euler(0,0,steering_angle)[1]
-		# model_state.pose.orientation.z = tf.transformations.quaternion_from_euler(0,0,steering_angle)[2]
-		# model_state.pose.orientation.w = tf.transformations.quaternion_from_euler(0,0,steering_angle)[3]
-		rospy.loginfo(model_state)
-		pub.publish(model_state)
-		rate.sleep()
+# 		model_state = ModelState()
+# 		model_state.model_name = "ackermann_vehicle"
+# 		model_state.pose = pose
+# 		model_state.twist = twist
+# 		model_state.reference_frame = "world"
+# 		# model_state.pose.orientation.x = tf.transformations.quaternion_from_euler(0,0,steering_angle)[0]
+# 		# model_state.pose.orientation.y = tf.transformations.quaternion_from_euler(0,0,steering_angle)[1]
+# 		# model_state.pose.orientation.z = tf.transformations.quaternion_from_euler(0,0,steering_angle)[2]
+# 		# model_state.pose.orientation.w = tf.transformations.quaternion_from_euler(0,0,steering_angle)[3]
+# 		rospy.loginfo(model_state)
+# 		pub.publish(model_state)
+# 		rate.sleep()
 
+def usage():
+	return "%s Enter two numbers speed and steering_angle"%sys.argv[0]
+ 
 if __name__ == '__main__':
 	try:
-		ackermann_publisher()
+		if len(sys.argv) == 3:
+			print "Sending to ackermann_cmd speed %s steering_angle %s)"%(sys.argv[1], sys.argv[2])
+			ackermann_publisher(sys.argv[1], sys.argv[2])
+		else:
+			print usage()
+			sys.exit(1)
 	except rospy.ROSInterruptException:
 		pass
