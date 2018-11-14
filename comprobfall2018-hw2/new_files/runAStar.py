f589a40f5ba40f1b651687f9cadad6c66cc60b33
Bennett Greenberg <bennett.greenberg@rutgers.edu>
2018-11-14 00:20:36 -0500

    finished updating runA* to 6D

--- comprobfall2018-hw2/new_files/runAStar.py
+++ comprobfall2018-hw2/new_files/runAStar.py
@@ -1,7 +1,6 @@
 import astar_fringe as fring
 import astar_node as anode
 import astar_closed as closed
-import astar_grid
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,10 +8,11 @@ import matplotlib
 import sys
 import PRM
 import RRT
+import planning
 
 #return heuristic of start while searching for goal (given by Equation 1 in assignment instructions). Or straight-line distance if running FDA*.
-def heur(startx, starty, starttheta, goalx, goaly, goaltheta):
-    return math.sqrt((startx-goalx)**2 + (starty-goaly)**2 + (starttheta-goaltheta)**2)
+def heur(config, goalConfig):
+    return planning.distance(config, goalConfig)
 
 #function to run the A* search algorithm from vertex start to vertex goal.
 def runAStar(Start, goal, grid, nodes, adjacency, distances):
@@ -41,18 +41,15 @@ def runAStar(Start, goal, grid, nodes, adjacency, distances):
                 break
         if s_ind == -1: sys.exit("Error: s_ind not found! s={}".format(str(s)))
         for ni in range(len(adjacency[s_ind])): #get k nearest neighbors of s: FIX THIS!!!!!(Which index for adjacency???)!
-            x = nodes[adjacency[s_ind][ni]][0] #neighbor's x value
-            y = nodes[adjacency[s_ind][ni]][1] #neighbor's y value
-        #    theta = nn.theta #does neighbor have theta value?
-            if grid.checkObstacle(s.x, s.y, x, y):
+            config = nodes[adjacency[s_ind][ni]].config #neighbor's configuration
+            if not planning.validPath(s.config, config)
                 continue
-            if not Closed.check(x, y, 0): #, theta):
-                if not fringe.check(x,y,0): #theta):
-                    n = anode.Node(x, y, 0, s, heur(x, y, 0, goal[0], goal[1], goal[2]))
+            if not Closed.check(config):
+                if not fringe.check(config): #theta):
+                    n = anode.Node(config, s, heur(config, goal)
                     fringe.insert(n)
               #update n so its parent is s.
-              #  print "s: {} s_ind: {} nodes[s_ind]: {} distances[s_ind]: {} adjacency[s_ind]: {}".format(s, s_ind, nodes[s_ind], distances[s_ind], adjacency[s_ind])
-                if fringe.update(s, x, y, 0, distances[s_ind][ni]):  #What is correct first index for distances?????????????????!!!!!!!!!!
+                if fringe.update(s, config, distances[s_ind][ni]):  #What is correct first index for distances?????????????????!!!!!!!!!!
                     updated = True
         if updated:
             fringe.updateFringeHeap()
@@ -92,18 +89,19 @@ def main():
     plt.yticks(np.arange(0, 5, 1))
     plt.grid(b=True)
     if not rrt:
-        twoDnodes, twoDadjacency, twoDdistances = PRM.PRM2D(nnodes)
-        new2Dnodes, new2Dadjacency, new2Ddistances, startIndex, goalIndex = PRM.startAndGoal2DPRM(twoDnodes, twoDadjacency, twoDdistances, (1.1, 2.2), (3.3, 4.4))
-        Start = anode.Node(new2Dnodes[startIndex][0], new2Dnodes[startIndex][1], 0, None, 0)
-        print "Start = " + str(Start.x) + ", " + str(Start.y)
-        Goal = (new2Dnodes[goalIndex][0], new2Dnodes[goalIndex][1], 0)
+        twoDnodes, twoDadjacency, twoDdistances = PRM.PianoPRM(nnodes)
+        twoDnodes, twoDadjacency, twoDdistances, startIndex, goalIndex = PRM.startAndGoal2DPRM(twoDnodes, twoDadjacency, twoDdistances, (1.1, 2.2, 0, 0, 0, 0), (3.3, 4.4, 0, 0, 0, 0))
+        Start = anode.Node(twoDnodes[startIndex].config, 0, None, 0)
+        Goal = (twoDnodes[goalIndex].config)
     else:
+        #FIX RRT
         twoDnodes, twoDadjacency = RRT.RRT2D(Start, (5,5), nnodes, 0.2)
         twoDdistances = [[0.2 for x in range(nnodes)] for y in range(nnodes)]
-    apath = runAStar(Start, Goal, grid, new2Dnodes, new2Dadjacency, new2Ddistances)
+    apath = runAStar(Start, Goal, grid, twoDnodes, twoDadjacency, twoDdistances)
     if rrt:
         RRT.RRT2Dshow(twoDnodes, twoDadjacency)
      #   plt.show()
+    #NOW MOVE ALONG THE PATH THAT WAS FOUND!
     points = []
     if not apath == None:
         #Now just follow the parent of goal in reverse to find the correct path.
@@ -116,21 +114,21 @@ def main():
             current = current.parent
         current = path.pop()
         while path != []:
-            if not ros: print("Turtlebot pos: x={}, y={}".format(current.x, current.y))
-            tbx.append(current.x)
-            tby.append(current.y)
+            if not ros: print("Position pos: x={}, y={}".format(current.config[0], current.config[1]))
+            tbx.append(current.config[0])
+            tby.append(current.config[1])
             current = path.pop()
             if ros:
                 import turtlebot_control_client
                  # add points to list to send to turtlebot_control_client
-                p = Point(current.x, current.y, 0)
+                p = Point(current.config[0], current.config[1], 0)
                 points.append(p)
-        if not ros: print("Turtlebot pos: x={}, y={}".format(current.x, current.y))
-        tbx.append(current.x)
-        tby.append(current.y)
+        if not ros: print("Position: x={}, y={}".format(current.config[0], current.config[1]))
+        tbx.append(current.config[0])
+        tby.append(current.config[1])
         plt.plot(tbx, tby)
-        plt.plot(grid.Start(num).x, grid.Start(num).y, marker='.', color='green', markersize=8)
-        plt.plot(grid.Goal(num)[0], grid.Goal(num)[1], marker='.', color='green', markersize=8)
+       # plt.plot(grid.Start(num).x, grid.Start(num).y, marker='.', color='green', markersize=8)
+       # plt.plot(grid.Goal(num)[0], grid.Goal(num)[1], marker='.', color='green', markersize=8)
     else:
         print("Error! No path found!")
     plt.show()
